32634615a4c8bd932479755c05220a5d7c977312
yogeshbhatt89 <61337526+yogeshbhatt89@users.noreply.github.com>
2025-04-27 17:59:43 -0400

    added health check service

--- src/main/java/com/investmentapp/investment_app/config/AppConfig.java
+++ src/main/java/com/investmentapp/investment_app/config/AppConfig.java
@@ -5,6 +5,7 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
 
 @Configuration
 public class AppConfig {
@@ -12,7 +13,12 @@ public class AppConfig {
     @Bean
     public ObjectMapper objectMapper() {
         return new ObjectMapper()
-                .registerModule(new JavaTimeModule()) // 🔹 Enable Java 8 LocalDateTime support
+                .registerModule(new JavaTimeModule())
                 .setSerializationInclusion(JsonInclude.Include.NON_NULL);
+    }
+
+    @Bean
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
     }
 }
