3e8f3641ac97310b5adcc17f5fc3ed8e0e53c740
Richard Lanchez <richardsunny2015@gmail.com>
2025-07-25 13:04:34 -0400

    Fix linting error

--- src/main/java/com/investmentapp/investment_app/config/SecurityConfig.java
+++ src/main/java/com/investmentapp/investment_app/config/SecurityConfig.java
@@ -3,6 +3,7 @@ package com.investmentapp.investment_app.config;
 import com.investmentapp.investment_app.repository.UserRepository;
 import com.investmentapp.investment_app.security.JwtAuthorizationFilter;
 import com.investmentapp.investment_app.security.JwtTokenUtil;
+import java.util.List;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
@@ -17,8 +18,6 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-
-import java.util.List;
 
 @Configuration
 @EnableWebSecurity
