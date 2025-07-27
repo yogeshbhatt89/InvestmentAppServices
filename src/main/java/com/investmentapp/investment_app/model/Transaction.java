453b99e12e8c3d06808e965ab339e9a200e3c3e8
yogeshbhatt89 <61337526+yogeshbhatt89@users.noreply.github.com>
2025-07-27 19:41:21 -0400

    Refactor

--- src/main/java/com/investmentapp/investment_app/model/Transaction.java
+++ src/main/java/com/investmentapp/investment_app/model/Transaction.java
@@ -3,12 +3,11 @@ package com.investmentapp.investment_app.model;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.investmentapp.investment_app.enums.TransactionType;
 import jakarta.persistence.*;
-import lombok.*;
-
 import java.io.Serial;
 import java.io.Serializable;
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
+import lombok.*;
 
 @Getter
 @Setter
